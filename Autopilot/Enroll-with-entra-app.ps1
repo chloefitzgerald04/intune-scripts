e58b0009820c01c60d88750aab042eb87769193a
C Fitzgerald <84927940+chloefitzgerald04@users.noreply.github.com>
2025-04-25 08:36:11 +0100

    Update Enroll-with-entra-app.ps1

--- Autopilot/Enroll-with-entra-app.ps1
+++ Autopilot/Enroll-with-entra-app.ps1
@@ -1,12 +1,17 @@
 
 
+$365_tenant_id = ""
+$365_entra_app_id = ""
+$365_entra_app_secret = ""
+$365_autopilot_grouptag = ""
+
 Install-Script -Name Get-WindowsAutopilotInfo -Force
 $AutopilotParams = @{
     Online = $true
-    TenantId = ''
-    AppId = ''
-    AppSecret = ''
-    GroupTag = ''
+    TenantId = '$365_tenant_id'
+    AppId = '$365_entra_app_id'
+    AppSecret = '$365_entra_app_secret'
+    GroupTag = '$365_autopilot_grouptag'
     Assign = $true
 }
 Get-WindowsAutoPilotInfo @AutopilotParams
